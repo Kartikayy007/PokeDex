8606a65ab944560acf29deed68db786404636c4f
kartikayy007 <kartikayy6969@gmail.com>
2025-09-18 22:47:38 +0530

    Initial Commit

---
+++ Pok-dex/Pok_dexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pok_dexApp.swift
+//  Pok-dex
+//
+//  Created by kartikay on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Pok_dexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
